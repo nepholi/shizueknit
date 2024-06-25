b9583402c13d8cc612931af75febe95754c238b9
Pow Lee <ptp_pow.lee@photons.com.tw>
2024-06-25 17:42:38 +0800

    [修改] 1.用戶名改為帳號 2.新增註冊時的格式檢查 3.用電子郵件判斷是否重複註冊

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"errors"
+	"fmt"
 	"html/template"
 	"net/http"
+	"regexp"
 	"shizueknit/internal/database"
 	"shizueknit/internal/models"
 	"shizueknit/internal/utils"
@@ -39,9 +42,14 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    username := r.FormValue("username")
+    account := r.FormValue("account")
     email := r.FormValue("email")
     password := r.FormValue("password")
+
+	if err := isUserRegistrationValidate(account, email, password); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+	}
 
     hashedPassword, err := utils.HashPassword(password)
     if err != nil {
@@ -50,7 +58,7 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
     }
 
     user := models.User{
-        Username: username,
+        Account: account,
         Email:    email,
         Password: hashedPassword,
     }
@@ -86,11 +94,11 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    username := r.FormValue("username")
+    account := r.FormValue("account")
     password := r.FormValue("password")
 
     var user models.User
-    result := database.DB.Where("username = ?", username).First(&user)
+    result := database.DB.Where("account = ?", account).First(&user)
     if result.Error != nil {
         http.Error(w, "Invalid credentials", http.StatusUnauthorized)
         return
@@ -118,4 +126,54 @@ func LogoutHandler(w http.ResponseWriter, r *http.Request) {
     }
 
     http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+// 帳號輸入欄位驗證
+func validateAccountField(account string) bool {
+    // 限制只能輸入英文及數字，大小寫皆可，上限為30字元
+    pattern := `^[a-zA-Z0-9]{1,30}$`
+    matched, _ := regexp.MatchString(pattern, account)
+    return matched
+}
+
+// 電子郵件輸入欄位驗證
+func validateEmailField(email string) bool {
+    // 檢查電子郵件格式是否正確
+    pattern := `^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$`
+    matched, _ := regexp.MatchString(pattern, email)
+    return matched
+}
+
+// 密碼輸入欄位驗證
+func validatePasswordField(password string) bool {
+    // 限制密碼最少8字元
+    return len(password) >= 8
+}
+
+// 檢查電子郵件是否已被註冊
+func isEmailRegistered(email string) bool {
+    // 在資料庫中查詢是否有相同的電子郵件
+	var user models.User
+	fmt.Println("isEmailRegistered:",email)
+	u := database.DB.Where("email = ?", email).First(&user)
+	fmt.Printf("%v",u)
+    return u!=nil
+}
+
+// 處理用戶註冊
+func isUserRegistrationValidate(account, email, password string) error {
+	fmt.Println("isUserRegistrationValidate")
+    if !validateAccountField(account) {
+        return errors.New("帳號格式不正確")
+    }
+    if !validateEmailField(email) {
+        return errors.New("電子郵件格式不正確")
+    }
+    if !validatePasswordField(password) {
+        return errors.New("密碼必須至少8個字元")
+    }
+    if isEmailRegistered(email) {
+        return errors.New("電子郵件已被註冊，請檢查")
+    }
+    return nil
 }
